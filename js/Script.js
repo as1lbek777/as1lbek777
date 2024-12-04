e5fbcc9da78212ffbee68559479ca33a4c34537d
asilbek <asilbekegamnazarov999@gmail.com>
2024-12-04 17:27:34 +0500

    vvvvv

--- js/Script.js
+++ js/Script.js
@@ -1,14 +1,4 @@
-//Object start
-const userData=[
-    "Sardorbek",
-    "Sayfullayev",
-    21,
-    ["Atham ","Jasur","Shahlo"]
-]
-const userInfo={
-    firstName:"Sardorbek",
-    lastName:"Sayfullayev",
-    age:21,
-    friends:["Atham ","Jasur","Shahlo"]
-}
-console.log(userInfo);
+// camel case usulida uzgaruvchilarni nomlash
+let firstName="Sardorbek";
+let secondName="Sayfullayev";
+let totalCost;
