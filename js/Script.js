9f7cba25cdd5be61da64ce8a2c50ddfe6fecacf2
asilbek <asilbekegamnazarov999@gmail.com>
2024-12-04 17:09:00 +0500

    vvvvv

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,14 @@
-console.log("Salom hamaga")
+//Object start
+const userData=[
+    "Sardorbek",
+    "Sayfullayev",
+    21,
+    ["Atham ","Jasur","Shahlo"]
+]
+const userInfo={
+    firstName:"Sardorbek",
+    lastName:"Sayfullayev",
+    age:21,
+    friends:["Atham ","Jasur","Shahlo"]
+}
+console.log(userInfo);
